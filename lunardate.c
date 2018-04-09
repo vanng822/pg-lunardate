ad86fa21e9cba137cc0d0bb64796e2841210322d
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2018-04-09 21:11:21 +0200

    Free memory as soon as we are done. Some formatting

--- lunardate.c
+++ lunardate.c
@@ -15,15 +15,14 @@ lunardate_in(PG_FUNCTION_ARGS) {
     solar_date *d;
     if (sscanf(str, "%u-%u-%u", &year, &month, &day) != 3) {
         ereport(ERROR,
-                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                 errmsg("invalid input syntax for lunardate: \"%s\"",
-                        str)));
+          (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+          errmsg("invalid input syntax for lunardate: \"%s\"", str)));
     }
     d = lunar2solar(day, month, year, 0, TIMEZONE);
     if (!IS_VALID_JULIAN(d->year, d->month, d->day)) {
-      ereport(ERROR,
-  				(errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
-  				 errmsg("date out of range: \"%s\"", str)));
+        ereport(ERROR,
+          (errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
+          errmsg("date out of range: \"%s\"", str)));
     }
     result = jd_from_date(d->day, d->month, d->year);
     //elog(INFO, "lunardate_in %d-%d-%d (jd %d)", d->year, d->month, d->day, result);
@@ -41,10 +40,10 @@ lunardate_out(PG_FUNCTION_ARGS) {
     //elog(INFO, "lunardate_out %d", jd);
     sdate = jd_to_date(jd);
     ldate = solar2lunar(sdate->day, sdate->month, sdate->year, TIMEZONE);
+    pfree(sdate);
     int size = 10 + VARHDRSZ;
     result = (char *) palloc(size);
     snprintf(result, size, "%04u-%02u-%02u", ldate->year, ldate->month, ldate->day);
-    pfree(sdate);
     pfree(ldate);
     PG_RETURN_CSTRING(result);
 }
@@ -53,7 +52,7 @@ PG_FUNCTION_INFO_V1(lunardate2date);
 Datum
 lunardate2date(PG_FUNCTION_ARGS) {
     int jd = PG_GETARG_INT32(0);
-    DateADT		date;
+    DateADT date;
     solar_date *sdate;
     sdate = jd_to_date(jd);
     date = date2j(sdate->year, sdate->month, sdate->day) - POSTGRES_EPOCH_JDATE;
